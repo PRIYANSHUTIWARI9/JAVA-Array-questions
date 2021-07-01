d380a3f8cb06220ec6f67738be4dcd4af3bc5784
PRIYANSHUTIWARI9 <tiwaripriyanshu22@gmail.com>
2021-07-01 23:16:23 +0530

    basicQuestion

---
+++ simpleBasic.java
@@ -0,0 +1,9 @@
+class simpleBasic{
+    public static void main(String[] args) {
+        int[] arr = {56,34,33};
+        for(int i=0; i<arr.length; i++)
+        {
+            System.out.println(arr[i]);
+        }
+    }
+}
