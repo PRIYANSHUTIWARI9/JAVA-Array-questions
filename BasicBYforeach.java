a52acc2d3e2f2bb4ceb6265a8e13212efcf9909a
PRIYANSHUTIWARI9 <tiwaripriyanshu22@gmail.com>
2021-07-01 23:19:14 +0530

    BasicQuestion2

---
+++ BasicBYforeach.java
@@ -0,0 +1,13 @@
+public class BasicBYforeach {
+   public static void main(String[] args) {
+    int arr[] = new int[3];
+    arr[0]=44;
+    arr[1]=23;
+    arr[2]=33;
+    for(int a:arr)
+    {
+        System.out.println(a);
+    }
+   }
+    
+}
