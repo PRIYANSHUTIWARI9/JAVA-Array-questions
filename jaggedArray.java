36b1a04b3f468d0f5f7b462d9232d9e6f804edba
PRIYANSHUTIWARI9 <tiwaripriyanshu22@gmail.com>
2021-07-02 14:03:25 +0530

    jagged Array

---
+++ jaggedArray.java
@@ -0,0 +1,27 @@
+public class jaggedArray {
+    public static void main(String[] args) {
+        int[][] arr = new int[3][];
+        arr[0] = new int[2];
+        arr[1] = new int[5];
+        arr[2] = new int[8];
+        int count=0;
+        for(int i=0; i<arr.length;i++)
+        {
+            for(int j=0; j<arr[i].length;j++)
+            {
+                arr[i][j]= count++;
+            }
+        }
+
+        for(int i=0; i<arr.length; i++)
+        {
+            for(int j=0; j<arr[i].length;j++)
+            {
+                System.out.print(arr[i][j]);
+            }
+            System.out.println();
+        }
+
+    }
+    
+}
