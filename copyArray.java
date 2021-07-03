ccd5e7fcac1710f9b3bb03546955b53c3877c422
PRIYANSHUTIWARI9 <tiwaripriyanshu22@gmail.com>
2021-07-03 12:23:10 +0530

    copy one array to another

---
+++ copyArray.java
@@ -0,0 +1,17 @@
+public class copyArray {
+    public static void main(String[] args) {
+        int[] arr1 = {2,4,5,6,7,9};
+        int[] arr2 = new int[arr1.length];
+
+        for(int i=0; i<arr2.length;i++)
+        {
+            arr2[i] = arr1[i];
+        }
+
+        for(int i=0; i<arr2.length;i++)
+        {
+            System.out.print(arr2[i]+ " ");
+        }
+     }
+    
+}
