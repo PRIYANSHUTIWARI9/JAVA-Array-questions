6ca86980231016788304cd2fcf7dfcc4d74ace78
PRIYANSHUTIWARI9 <tiwaripriyanshu22@gmail.com>
2021-07-04 17:19:54 +0530

    descending array

---
+++ descending.java
@@ -0,0 +1,26 @@
+public class descending {
+    public static void main(String[] args) {
+        int[] arr = {1,3,6,7,9,2};
+        int temp=0;
+        for(int i=0;i<arr.length;i++)
+        {
+            for(int j=i+1;j<arr.length;j++)
+            {
+                if(arr[i]<arr[j])
+                {
+                    temp= arr[i];
+                    arr[i] = arr[j];
+                    arr[j] = temp;
+                }
+            }
+        }
+        System.out.println();
+
+        for(int i=0;i<arr.length;i++)
+        {
+            System.out.print(arr[i]+" ");
+        }
+    
+    }
+    
+}
